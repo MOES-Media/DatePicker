65f4ee751ae38baa1510240c71eb4d530a20a5da
Jeroen Nouws <jeroennouwsdev@gmail.com>
2017-12-05 22:02:06 +0100

    added some code before travis config

---
+++ src/component/index.js
@@ -0,0 +1,10 @@
+//@flow
+import React, { Component } from 'react'
+import { DatePickerProps } from 'component/types'
+
+export default class extends Component<DatePickerProps>{
+
+    render(){
+        return(<h1>Hello DatePicker</h1>)
+    }
+}
---
+++ src/component/types.js
@@ -0,0 +1,4 @@
+//@flow
+export type DatePickerProps = {
+    isCalendarVisible: boolean,
+}
