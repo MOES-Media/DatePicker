2905466b0ee76bf8f7f0ad85c21c37fb3a3e7913
Jeroen Nouws <jeroennouwsdev@gmail.com>
2017-12-08 12:58:39 +0100

    test fixing?

--- src/__test__/utils/dates.spec.js
+++ src/__test__/utils/dates.spec.js
@@ -248,8 +248,8 @@ describe('utils: dates', () => {
         })
 
         it('should return a parsed date when passing in a valid dateString', () => {
-            expect(dateUtils.parseDates('06/12/2017T00:00:00', { dateFormat: 'DD/MM/YYYY' }).toDate().getTime())
-                .toBe(dateUtils.newDate('2017-12-06T00:00:00').toDate().getTime())
+            expect(dateUtils.setUtcOffSet(dateUtils.parseDates('06/12/2017', { dateFormat: 'DD/MM/YYYY' }), 0).toDate().getTime())
+                .toBe(dateUtils.setUtcOffSet(dateUtils.newDate('2017-12-06'), 0).toDate().getTime())
         })
 
         it('should return null when passing a valid dateString that doesn\'t match the pattern and strict is true', () => {
